3d2fec3b6e6924eb597e06fd6b08af0fa2215b1a
Nick Forma <nickforma60@gmail.com>
2019-08-07 18:36:26 -0400

    initial links refs

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    
+    
+    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDgzgfmdBJM5QCbuD70TzQ3HoAtYzpf6NM",
+        authDomain: "trainsched-581b6.firebaseapp.com",
+        databaseURL: "https://trainsched-581b6.firebaseio.com",
+        projectId: "trainsched-581b6",
+        storageBucket: "",
+        messagingSenderId: "312542928293",
+        appId: "1:312542928293:web:77d3f2b8b3f477c6"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    
+    
+    
+
+
+
+
+
+
+    
+    });
