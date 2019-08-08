f27e3eade8e8b417ebbdc902bcf4b8edba34f122
Nick Forma <nickforma60@gmail.com>
2019-08-07 21:57:24 -0400

    moment.js and firebase

--- js/index.js
+++ js/index.js
@@ -1,29 +1,20 @@
-$(document).ready(function(){
-    
-    
-    
-    
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-        apiKey: "AIzaSyDgzgfmdBJM5QCbuD70TzQ3HoAtYzpf6NM",
-        authDomain: "trainsched-581b6.firebaseapp.com",
-        databaseURL: "https://trainsched-581b6.firebaseio.com",
-        projectId: "trainsched-581b6",
-        storageBucket: "",
-        messagingSenderId: "312542928293",
-        appId: "1:312542928293:web:77d3f2b8b3f477c6"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    
-    
-    
-    
+$(document).ready(function() {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDgzgfmdBJM5QCbuD70TzQ3HoAtYzpf6NM",
+    authDomain: "trainsched-581b6.firebaseapp.com",
+    databaseURL: "https://trainsched-581b6.firebaseio.com",
+    projectId: "trainsched-581b6",
+    storageBucket: "",
+    messagingSenderId: "312542928293",
+    appId: "1:312542928293:web:77d3f2b8b3f477c6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
+  let database = firebase.databse();
 
-
-
-
-
-    
-    });
+  database.ref().on(function(snapshot) {
+    console.log(snapshot.val());    
+  });
+});
